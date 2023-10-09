da739e58d578a4cab5c4b2f678d8cef97c91d613
lucieruin <lucie.wattez@hotmail.fr>
2023-10-09 14:56:04 +0200

    a class Square that defines a square with raise exceptions

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module 2
+"""
+
+
+class Square:
+    """define  raise exception
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
