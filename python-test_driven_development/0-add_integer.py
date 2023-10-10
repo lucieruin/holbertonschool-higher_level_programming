90049e686a8750ee976ce86bea4e3c0f13101d7d
lucieruin <lucie.wattez@hotmail.fr>
2023-10-10 14:38:45 +0200

    add comments for the function

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
+
+""" add 2 integers """
+
+
 def add_integer(a, b=98):
+    """ args:
+            a(int): number
+            b(int): number set defauts to 98
+
+        raises:
+            TypeError: if  or b is not an int or a float
+
+        Return: result(a+b)
+    """
+
     if isinstance(a, (int, float)):
         a = int(a)
     else:
@@ -8,5 +22,5 @@ def add_integer(a, b=98):
         b = int(b)
     else:
         raise TypeError("b must be an integer")
-    
+
     return a + b
