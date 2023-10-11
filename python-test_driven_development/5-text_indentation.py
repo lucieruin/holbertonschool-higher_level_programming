7807e86506a0d32d6ede02366614cb87dd693941
lucieruin <lucie.wattez@hotmail.fr>
2023-10-11 14:42:32 +0200

    prints a text with 2 new lines after each of these characters: ., ? and : & add test file for this function

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+""" print a text with two line after ., ? and : """
+
+
+def text_indentation(text):
+    """ Args:
+            text: text to be indented
+
+        raises:
+            TypeError: if text is not a string
+    """
+
+    newText = ""
+    needLineBreak = False
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    newText = text.replace(". ", ".")
+    newText = text.replace("? ", "?")
+    newText = text.replace(": ", ":")
+    for char in newText:
+        if char in [".", "?", ":"]:
+            print(char)
+            print()
+            needLineBreak = True
+        else:
+            if needLineBreak is False:
+                print(char, end="")
+            else:
+                if char != " ":
+                    print(char, end="")
+                    needLineBreak = False
