42dc2bdbd96875a3c0ab100d7f97fd22ccd75ae5
lucieruin <lucie.wattez@hotmail.fr>
2023-10-17 13:12:05 +0200

    return true or false

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" returns True  otherwise False """
+
+
+def inherits_from(obj, a_class):
+    """returns True otherwise false
+
+    Args:
+        obj (any): object to check
+        a_class (any): given an object
+
+    Return: True if object in instance otherwise false
+    """
+
+    return isinstance(obj, a_class) or not issubclass(a_class, obj.__class__)
