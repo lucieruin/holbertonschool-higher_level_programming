0e26c8e0a2b1e04caca218a5743c5898aff089fe
lucieruin <lucie.wattez@hotmail.fr>
2023-10-18 12:41:25 +0200

    append a text

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" append a text """
+
+
+def append_write(filename="", text=""):
+    """ append a text """
+
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
