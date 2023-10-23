a47ad1eaef43544da87bfd996deb30116cca8a94
lucieruin <lucie.wattez@hotmail.fr>
2023-10-23 18:12:05 +0200

    Write the class Rectangle that inherits from Base

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+""" class Rectangle that inherits from Base """
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ class rectangle """
+
+    @property
+    def width(self):
+        """ width getter method """
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ width setter method """
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """ height getter method """
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height setter method """
+
+        self.__height = value
+
+    @property
+    def x(self):
+        """ x getter method """
+
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ x setter method """
+
+        self.__x = value
+
+    @property
+    def y(self):
+        """ y getter method """
+
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ y setter method """
+
+        self.__y = value
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """instance initialization method
+
+        Args:
+            width: width of rectangle
+            height: height of rectangle
+            x: init variable
+            y: init variable
+        """
+
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
