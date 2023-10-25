fa612a7cbb080792ba04a48ae1e0091f653d50dd
lucieruin <lucie.wattez@hotmail.fr>
2023-10-25 14:20:17 +0200

    add representation in dictionnary

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -99,3 +99,8 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ dixtionnary representation """
+        return {'x': self.x, 'y': self.y, 'id': self.id, 'height': self.height,
+                'width': self.width}
