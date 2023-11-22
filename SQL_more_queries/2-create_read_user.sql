85279642b6c8df1b6c5a7daccf551bd2a6b888d6
lucieruin <lucie.wattez@hotmail.fr>
2023-11-22 13:13:00 +0100

    chage privilege name and add precision for database

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -5,4 +5,4 @@ CREATE DATABASE IF NOT EXISTS user_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- add privilege for this user
-GRANT VIEW ON 'hbtn_0d_2' TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
