7b73efe442778da9dc1a1540d13b18fdebfb2b34
lucieruin <lucie.wattez@hotmail.fr>
2023-11-22 14:10:57 +0100

    changed in lower the word name

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -6,5 +6,5 @@ USE hbtn_0d_usa
 -- creates a table on this database
 CREATE TABLE IF NOT EXISTS states (
 id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-NAME VARCHAR(256) NOT NULL
+name VARCHAR(256) NOT NULL
 );
