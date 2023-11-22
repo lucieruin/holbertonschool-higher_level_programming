6983a1f5f97f592f49597fbe299a35afce19da7f
lucieruin <lucie.wattez@hotmail.fr>
2023-11-22 13:09:12 +0100

    create a database and an user with privilege to read this database

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- creates the database hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS user_0d_2;
+
+-- creates he user user_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- add privilege for this user
+GRANT VIEW ON 'hbtn_0d_2' TO 'user_0d_2'@'localhost';
