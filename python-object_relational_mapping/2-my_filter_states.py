a8e8e36967b51ad9f78121f5e7900dc9ad927143
lucieruin <lucie.wattez@hotmail.fr>
2023-11-28 13:42:16 +0100

    takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument """
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    database = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+
+    cursor = database.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' 'N%' ORDER BY id ASC"
+    )
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    database.close()
