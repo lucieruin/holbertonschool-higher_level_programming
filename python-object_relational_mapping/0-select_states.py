7858fb0686ecb67254f53e3c84542f80c8693f14
lucieruin <lucie.wattez@hotmail.fr>
2023-11-28 12:04:23 +0100

    supp function def

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,14 +5,22 @@ import MySQLdb
 import sys
 
 
-def list_states(MySQL_username, MySQL_password, MySQL_database):
-    """ connect to MySQL server """
+if __name__ == "__main__":
+    """Check if the correct number of command-line arguments are provided"""
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+        sys.exit(1)
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=MySQL_username,
-        passwd=MySQL_password,
-        db=MySQL_database
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
     )
     cursor = db.cursor()
     query = "SELECT * FROM states ORDER BY states.id ASC"
@@ -22,12 +30,3 @@ def list_states(MySQL_username, MySQL_password, MySQL_database):
         print(row)
     cursor.close()
     db.close()
-
-
-if __name__ == "__main__":
-    """Check if the correct number of command-line arguments are provided"""
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database>")
-        sys.exit(1)
-    MySQL_username, MySQL_password, MySQL_database = sys.argv[1:4]
-    list_states(MySQL_username, MySQL_password, MySQL_database)
