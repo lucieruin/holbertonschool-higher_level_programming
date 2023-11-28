b99b2259fd3286916555d33ea224d8fe912de677
lucieruin <lucie.wattez@hotmail.fr>
2023-11-28 13:33:21 +0100

    add LIKE BINARY for just uppercase N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,9 @@ if __name__ == "__main__":
     )
 
     cursor = database.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    )
 
     rows = cursor.fetchall()
 
