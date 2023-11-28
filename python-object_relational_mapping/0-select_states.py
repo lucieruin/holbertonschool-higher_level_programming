88fe0917cfafb4f9f10ccb1bbaeb1c1ac6928665
lucieruin <lucie.wattez@hotmail.fr>
2023-11-28 12:12:58 +0100

    add connection

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,25 +8,25 @@ import sys
 if __name__ == "__main__":
     """Check if the correct number of command-line arguments are provided"""
     if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database>")
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-    db = MySQLdb.connect(
+    connection = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=mysql_username,
         passwd=mysql_password,
         db=database_name
     )
-    cursor = db.cursor()
+    cursor = connection.cursor()
     query = "SELECT * FROM states ORDER BY states.id ASC"
     cursor.execute(query)
     results = cursor.fetchall()
     for row in results:
         print(row)
     cursor.close()
-    db.close()
+    connection.close()
